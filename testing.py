77abd0e0d6ecfc7f624b53209427b28b1bf36b1d
liyb2002 <williamliyuanbo@gmail.com>
2025-03-26 16:18:43 -0400

    example done

--- testing.py
+++ testing.py
@@ -1,12 +1,20 @@
 from build123d import *
 from build123d import export_stl
 
-length, width, thickness = 60.0, 60.0, 60.0
+with BuildPart() as part:
+    # Step 1: Make a cylinder by extruding a circle
+    with BuildSketch():
+        Circle(10)
+    extrude(amount=20)  # cylinder height is 20
 
-with BuildPart() as ex2:
-    Box(length, width, thickness)
+    # Step 2: Sketch a rectangle on the side face (we pick one of the vertical faces)
+    side_face = part.faces().filter_by(Axis.Z)[0]
 
+    with BuildSketch(Plane(side_face)):
+        Rectangle(10, 10, align=(Align.CENTER, Align.CENTER))
+    
+    # Step 3: Extrude the rectangle outward by 5mm
+    # extrude(amount=-5, mode=Mode.ADD)
+    extrude(amount=-5, mode=Mode.SUBTRACT)
 
-
-
-export_stl(ex2.part, "file.stl")
+export_stl(part.part, "file.stl")
