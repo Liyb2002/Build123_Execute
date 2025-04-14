f37694df8e779dbf00821f2f1beabafa64613c8e
liyb2002 <williamliyuanbo@gmail.com>
2025-04-13 22:42:59 -0400

    nice helper

--- testing.py
+++ testing.py
@@ -10,11 +10,20 @@ with BuildPart() as part:
     # Step 2: Sketch a rectangle on the side face (we pick one of the vertical faces)
     side_face = part.faces().filter_by(Axis.Z)[0]
 
+
+
+    with BuildSketch(Plane(side_face)):
+        Rectangle(10, 10, align=(Align.CENTER, Align.CENTER))
+
+    extrude(amount=5, mode=Mode.SUBTRACT)
+
+
     with BuildSketch(Plane(side_face)):
         Rectangle(10, 10, align=(Align.CENTER, Align.CENTER))
     
     # Step 3: Extrude the rectangle outward by 5mm
-    # extrude(amount=-5, mode=Mode.ADD)
-    extrude(amount=-5, mode=Mode.SUBTRACT)
+    extrude(amount=5, mode=Mode.ADD)
+
 
 export_stl(part.part, "file.stl")
+export_step(part.part, "file.step")
