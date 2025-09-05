bbe18a6b98a5a99bdd3d098ac49fe614b3f0ad8b
Yuanbo Li <55519796+Liyb2002@users.noreply.github.com>
2025-09-05 03:38:27 -0400

    clean up

--- testing.py
+++ testing.py
@@ -1,29 +1,23 @@
 from build123d import *
-from build123d import export_stl
+from build123d import export_stl, export_step
 
 with BuildPart() as part:
-    # Step 1: Make a cylinder by extruding a circle
-    with BuildSketch():
-        Circle(10)
-    extrude(amount=20)  # cylinder height is 20
+    # Step 1: Make cuboid with height=0.2, length=0.5, width=1.0
+    with BuildPart() as base:
+        Box(0.5, 1.0, 0.2, align=(Align.MIN, Align.MIN, Align.MIN))
 
-    # Step 2: Sketch a rectangle on the side face (we pick one of the vertical faces)
-    side_face = part.faces().filter_by(Axis.Z)[0]
+    # Step 2: Cut a cuboid on the right corner, height=0.2, length=0.1, width=0.6
+    with BuildPart(part.part, mode=Mode.SUBTRACT):
+        Box(0.1, 0.6, 0.2, align=(Align.MAX, Align.MIN, Align.MIN))
 
-
-
-    with BuildSketch(Plane(side_face)):
-        Rectangle(10, 10, align=(Align.CENTER, Align.CENTER))
-
-    extrude(amount=5, mode=Mode.SUBTRACT)
-
-
-    with BuildSketch(Plane(side_face)):
-        Rectangle(10, 10, align=(Align.CENTER, Align.CENTER))
-    
-    # Step 3: Extrude the rectangle outward by 5mm
-    extrude(amount=5, mode=Mode.ADD)
-
+    # Step 3: Cut two cylinders (as holes) near the back, height=0.2, radius=0.08
+    with BuildPart(part.part, mode=Mode.SUBTRACT):
+        # First hole
+        Cylinder(radius=0.08, height=0.2, align=(Align.CENTER, Align.MIN, Align.MIN),
+                 mode=Mode.SUBTRACT).locate(Location((0.15, 0.9, 0)))
+        # Second hole
+        Cylinder(radius=0.08, height=0.2, align=(Align.CENTER, Align.MIN, Align.MIN),
+                 mode=Mode.SUBTRACT).locate(Location((0.35, 0.9, 0)))
 
 export_stl(part.part, "file.stl")
 export_step(part.part, "file.step")
